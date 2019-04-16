7ae1e2b01a5ed826acf0fa436bf59ad559c45100
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-04-16 20:39:02 +0300

    first lesson

---
+++ src/com/javaguru/lessons/lesson1/MathOperations.java
@@ -0,0 +1,17 @@
+package com.javaguru.lessons.lesson1;
+
+public class MathOperations {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
+        int sumResult = a + b;
+        System.out.println("Sum result: " + sumResult);
+
+        int multiplyResult = a * b;
+        System.out.println("Multiply result: " + multiplyResult);
+
+        double divideResult = a / (double) b;
+        System.out.println("Divide result: " + divideResult);
+    }
+}
---
+++ src/com/javaguru/lessons/lesson1/Variables.java
@@ -0,0 +1,21 @@
+package com.javaguru.lessons.lesson1;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        double salary = 20000.01;
+        System.out.println("Salary: " + salary + " Euro");
+
+        int age = 18;
+        System.out.println("Age: " + age);
+
+        boolean hungry = true;
+        System.out.println("Hungry: " + hungry);
+
+        char star = '*';
+        System.out.println("Star: " + star);
+
+        char letterA = 65;
+        System.out.println("Letter: " + letterA);
+    }
+}
